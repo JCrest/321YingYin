4804faf6966ab4f81911e492ca5cf4c5d4f464f3
JCrest <1554687045@qq.com>
2017-05-19 15:20:30 +0800

    各种修改后

---
+++ app/src/main/java/com/example/jiangchuanfa/a321yingyin/fragment/LocalVideoFragment.java
@@ -0,0 +1,34 @@
+package com.example.jiangchuanfa.a321yingyin.fragment;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.jiangchuanfa.a321yingyin.base.BaseFragment;
+
+/**
+ * Created by crest on 2017/5/19.
+ */
+
+public class LocalVideoFragment extends BaseFragment {
+    private TextView textView;
+    @Override
+    public View initView() {
+        Log.e("TAG","本地音频ui初始化了。。");
+        textView = new TextView(mContext);
+        textView.setTextColor(Color.RED);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(25);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        Log.e("TAG","本地音频数据初始化了。。");
+        textView.setText("本地视频");
+    }
+}
+
